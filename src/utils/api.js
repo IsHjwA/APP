a9cbf3acf9e944844420b36732ffe154bb98d04a
geekxia <448914712@qq.com>
2020-06-20 15:57:05 +0800

    20

---
+++ src/utils/api.js
@@ -0,0 +1,125 @@
+import fetch from './fetch'  // fetch实际上一个axios实例（promise对象）
+
+// ES6 模块：任何一个js文件都可以看成是一个独立作用域的模块
+
+// 第一种写法
+// export aaa
+// export bbb
+// import { aaa, bbb } from './xxx.js'
+
+// 第二种写法：在同一个模块中，最多只能使用一次 export default
+// export default
+// import ccc from './xxx.js'
+
+// 注册接口
+export function regist(data) {
+  return fetch({
+    url: '/user/regist',
+    method: 'POST',
+    data
+  })
+}
+// 登录接口
+export function login(data) {
+  return fetch({
+    url: '/user/login',
+    method: 'POST',
+    data
+  })
+}
+
+// 获取首页为你推荐的商品列表
+export function getHotGoodList(params) {
+  return fetch({
+    url: '/jd/getHotGoodList',
+    method: 'GET',
+    params
+  })
+}
+
+// 获取商品详情
+export function getGoodDetail(params) {
+  return fetch({
+    url: '/jd/getGoodDetail',
+    method: 'GET',
+    params
+  })
+}
+
+// 加入购物车
+export function addToCart(data) {
+  return fetch({
+    url: '/jd/addToCart',
+    method: 'POST',
+    data
+  })
+}
+
+// 获取购物车列表
+export function getCartList(params) {
+  return fetch({
+    url: '/jd/getCartList',
+    method: 'GET',
+    params
+  })
+}
+
+// 删除一条购物记录
+export function deleteCart(params) {
+  return fetch({
+    url: '/jd/deleteToCart',
+    method: 'GET',
+    params
+  })
+}
+
+// 修改购物车中商品的数量
+export function updateCart(data) {
+  return fetch({
+    url: '/jd/updateCartNum',
+    method: 'POST',
+    data
+  })
+}
+
+// 提交购物车
+export function submitCart(data) {
+  return fetch({
+    url: '/jd/submitToCart',
+    method: 'POST',
+    data
+  })
+}
+
+// 获取所有品类
+export function getAllCates(params) {
+  return fetch({
+    url: '/jd/getAllCates',
+    method: 'GET',
+    params
+  })
+}
+
+// 基于品类进行筛选
+export function getGoodOfCate(params) {
+  return fetch({
+    url: '/jd/getCateGoodList',
+    method: 'GET',
+    params
+  })
+}
+
+// 把所有接口方法统一抛出
+export default {
+  regist,
+  login,
+  getHotGoodList,
+  getGoodDetail,
+  addToCart,
+  getCartList,
+  deleteCart,
+  updateCart,
+  submitCart,
+  getAllCates,
+  getGoodOfCate
+}
